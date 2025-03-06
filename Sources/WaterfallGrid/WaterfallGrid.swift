a71a6f28421d99337c1a029fe8f8a4b79fd557cd
liyb <libcm93@gmail.com>
2025-03-06 10:20:01 +0800

    Update data as binding data

--- Sources/WaterfallGrid/WaterfallGrid.swift
+++ Sources/WaterfallGrid/WaterfallGrid.swift
@@ -13,7 +13,7 @@ public struct WaterfallGrid<Data, ID, Content>: View where Data : RandomAccessCo
     @Environment(\.gridStyle) private var style
     @Environment(\.scrollOptions) private var scrollOptions
 
-    private let data: Data
+    @Binding var data: Data
     private let dataId: KeyPath<Data.Element, ID>
     private let content: (Data.Element) -> Content
 
@@ -105,8 +105,8 @@ extension WaterfallGrid {
     /// - Parameter data: A collection of data.
     /// - Parameter id: Key path to a property on an underlying data element.
     /// - Parameter content: A function that can be used to generate content on demand given underlying data.
-    public init(_ data: Data, id: KeyPath<Data.Element, ID>, content: @escaping (Data.Element) -> Content) {
-        self.data = data
+    public init(_ data: Binding<Data>, id: KeyPath<Data.Element, ID>, content: @escaping (Data.Element) -> Content) {
+        self._data = data
         self.dataId = id
         self.content = content
     }
@@ -120,8 +120,8 @@ extension WaterfallGrid where ID == Data.Element.ID, Data.Element : Identifiable
     ///
     /// - Parameter data: A collection of identified data.
     /// - Parameter content: A function that can be used to generate content on demand given underlying data.
-    public init(_ data: Data, content: @escaping (Data.Element) -> Content) {
-        self.data = data
+    public init(_ data: Binding<Data>, content: @escaping (Data.Element) -> Content) {
+        self._data = data
         self.dataId = \Data.Element.id
         self.content = content
     }
